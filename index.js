0bd1d4dfd9ecd17f7a77561de32a5ad84ba9d31a
MichaellIbarra <131507781+MichaellIbarra@users.noreply.github.com>
2023-12-04 20:59:05 -0500

    .

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const path = require('path');
-
 const app = express();
 const port = 3000;
 
@@ -13,85 +10,52 @@ const db = mysql.createConnection({
   password: '',
   database: 'InstitutoValleGrande'
 });
-
 db.connect((err) => {
-  if (err) {
-    console.error('Error de conexión a la base de datos: ' + err.stack);
-    return;
-  }
+  if (err) throw err;
   console.log('Conexión exitosa a la base de datos');
 });
 
 // Configuración de Express
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
-// Configuración de EJS como motor de plantillas
+// Configuración de vista EJS 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-// Rutas
+// Rutas de renderizar
 app.get('/', (req, res) => {
   res.render('login');
 });
-
 app.get('/registro', (req, res) => {
   res.render('registro');
 });
 
-
 app.post('/registro', (req, res) => {
-    const { carrera, nombres, apellidos, dni, fecha_nacimiento, correo_institucional, contrasena, terminos_condiciones } = req.body;
-
-    // Validar los datos en el servidor (por si se intenta enviar la solicitud sin ejecutar JavaScript)
-    if (!carrera || !nombres || !apellidos || !dni || !fecha_nacimiento || !correo_institucional || !contrasena || !terminos_condiciones) {
-        res.send('Todos los campos deben estar completos.');
-        return;
-    }
-    if (!correo_institucional.endsWith('@vallegrande.edu.pe')) {
-        res.send('El correo institucional debe terminar con @vallegrande.edu.pe');
-        return;
-    }
-    if (dni.length !== 8 || isNaN(dni)) {
-        res.send('El número de DNI debe tener 8 dígitos.');
-        return;
-    }
-    const sql = "INSERT INTO Estudiantes (carrera, nombres, apellidos, dni, fecha_nacimiento, correo_institucional, contrasena) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    db.query(sql, [carrera, nombres, apellidos, dni, fecha_nacimiento, correo_institucional, contrasena], (err, result) => {
-        if (err) {
-            console.error('Error al registrar estudiante: ' + err.message);
-            res.redirect('/registro-error');
-        } else {
-            res.redirect('login');
-        }
-    });
-});
-
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-
-app.post('/login', (req, res) => {
-    const { correo_institucional, contrasena } = req.body;
-    const sql = "SELECT * FROM Estudiantes WHERE correo_institucional = ? AND contrasena = ?";
-    db.query(sql, [correo_institucional, contrasena], (err, results) => {
-      if (err) {
-        console.error('Error al realizar la consulta: ' + err.message);
-        res.redirect('/login-error');
-      } else {
-        if (results.length > 0) {
-          const estudiante = results[0];
-          res.render('panel', { estudiante });
-        } else {
-          res.redirect('/login-error');
-        }
-      }
-    });
+  const { carrera, nombres, apellidos, dni, fecha_nacimiento, correo_institucional, contrasena } = req.body;
+  const sql = "INSERT INTO Estudiantes (carrera, nombres, apellidos, dni, fecha_nacimiento, correo_institucional, contrasena) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  db.query(sql, [carrera, nombres, apellidos, dni, fecha_nacimiento, correo_institucional, contrasena], (err, result) => {
+    if (err) throw err;
+    res.redirect('/');
   });
+});
+
+app.post('/panel', (req, res) => {
+  const { correo_institucional, contrasena } = req.body;
+  const sql = "SELECT * FROM Estudiantes WHERE correo_institucional = ? AND contrasena = ?";
+  db.query(sql, [correo_institucional, contrasena], (err, results) => {
+    if (err) throw err;
+    if (results.length > 0) {
+      const estudiante = results[0];
+      console.log(estudiante);
+      res.render('panel', { estudiante });
+    } else {
+      res.redirect('/');
+    }
   
+  });
+});
+
 // Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
